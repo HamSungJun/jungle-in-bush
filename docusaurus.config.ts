09b9760cdaa821c30e050f3619bd11cdf0528d81
HamSungJun <tjdwns5123@gmail.com>
2024-12-14 01:25:24 +0900

    chore: 소셜 카드 변경

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/facebook_cover_photo_2.png',
     navbar: {
       title: '세상꾼 H',
       logo: {
