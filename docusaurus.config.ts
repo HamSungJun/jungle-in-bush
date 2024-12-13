81482ef915f0005d8c8638834e070cfc24ad691b
HamSungJun <tjdwns5123@gmail.com>
2024-12-14 01:09:07 +0900

    feat: baseurl 변경

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://jungle-in-bush.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/jungle-in-bush/',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
